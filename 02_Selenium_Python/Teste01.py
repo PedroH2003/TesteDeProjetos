6510e749ae2a63ef1d558e5337aed305b89859d6
PedroH2003 <pedrohenriquecandidosilva14@gmail.com>
2023-11-03 18:03:56 -0300

    testando a biblioteca selenium do python

---
+++ 02_Selenium_Python/Teste01.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+servico = Service(ChromeDriverManager().install())
+
+navegador = webdriver.Chrome(service=servico)
+
+navegador.get("https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M")
+
+navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys("Pedro Henrique")
+navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys("pedrohenriquecandidosilva14@gmail.com")
+navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[3]/div/input').send_keys("34987197164")
+navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/button/span/b').click()
+
+a = input() #só para o navegador não fechar instantaneamente ao executar os seus comandos
